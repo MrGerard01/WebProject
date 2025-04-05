fe2bfc02cb550626984dd11e7ecfb92a833c618e
Bogdan2005-19 <stefbogdanicolae@gmail.com>
2025-04-06 00:17:47 +0200

    Experimentacion

--- web/urls.py
+++ web/urls.py
@@ -3,9 +3,11 @@ from web import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("genero/<str:category>/", views.home, name="games_by_genre"),  # Ruta con género
+    path("home/<str:category>/", views.home, name="games_by_genre"),  # Ruta con género
     path("juego/<int:pk>/", views.game, name="game"),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('games/', views.game_list, name='game_list'),
+    path('games/<str:category>', views.game_list, name='game_list'),
 ]
--- web/views.py
+++ web/views.py
@@ -3,33 +3,18 @@ from django.core.paginator import Paginator
 from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.text import slugify
-
 from web.forms import AuthenticationForm, RegistrationForm
 from web.models import Videojuego, Genero, Usuario
 
 
 def home(request, category=None):
     """Lista de videojuegos, filtrados por género si se proporciona uno"""
-
-    print(f"Category recibido en slug: {category}")
-
     videojuegos = Videojuego.objects.all().order_by('titulo')
     generos = Genero.objects.all()
 
-    if category:
-        try:
-            # Buscar en la base de datos el género con el slug coincidente
-            genero = next((g for g in generos if slugify(g.nombre) == category), None)
-            if genero:
-                videojuegos = videojuegos.filter(genero=genero)
-                category = genero.nombre  # Restaurar el nombre original del género
-            else:
-                videojuegos = Videojuego.objects.none()
-        except Exception as e:
-            print(f"Error al buscar género: {e}")
-            videojuegos = Videojuego.objects.none()
+    videojuegos, category = filtrar_genero(videojuegos, category, generos)
 
-    game_pagination = Paginator(videojuegos, 32)
+    game_pagination = Paginator(videojuegos, 20)
     page_number = request.GET.get('page')
     page_obj = game_pagination.get_page(page_number)
 
@@ -42,6 +27,43 @@ def home(request, category=None):
 def game(request, pk):
     juego = get_object_or_404(Videojuego, pk=pk)
     return render(request, "game.html", {"juego": juego})
+
+def game_list(request, category=None):
+    query = request.GET.get('q')  # Obtiene lo que el usuario escribió
+
+    videojuegos = Videojuego.objects.all().order_by('titulo')
+    generos = Genero.objects.all().order_by('nombre')
+
+    if query:
+        videojuegos = Videojuego.objects.filter(titulo__icontains=query)
+
+    videojuegos, category = filtrar_genero(videojuegos, category, generos)
+
+    game_pagination = Paginator(videojuegos, 20)
+    page_number = request.GET.get('page')
+    page_obj = game_pagination.get_page(page_number)
+
+    return render(request, 'Game_list.html', {
+        'page_obj': page_obj,
+        "generos": generos,
+        'query': query,
+        "genero_actual": category,
+    })
+
+def filtrar_genero(videojuegos, category, generos):
+    if category:
+        try:
+            # Buscar en la base de datos el género con el slug coincidente
+            genero = next((g for g in generos if slugify(g.nombre) == category), None)
+            if genero:
+                videojuegos = videojuegos.filter(genero=genero)
+                category = genero.nombre  # Restaurar el nombre original del género
+            else:
+                videojuegos = Videojuego.objects.none()
+        except Exception as e:
+            print(f"Error al buscar género: {e}")
+            videojuegos = Videojuego.objects.none()
+    return videojuegos, category
 
 def login_view(request):
     if request.method == 'POST':
